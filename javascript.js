96f671fd902edc44d2cadf852eed4f85547eb3c4
Voloshchenko <voloshchenko.o@hotmail.com>
2019-02-23 19:55:39 +0300

    design

--- javascript.js
+++ javascript.js
@@ -3,9 +3,8 @@ document.addEventListener("scroll", inTheView)
 function inTheView(){
 let newBlock = document.getElementsByClassName("container")
 	for(let i=0; i<newBlock.length; i++){
-		let blockLocation = newBlock[i].getBoundingClientRect();
-		console.log(blockLocation)		
-			if(0 < blockLocation.top && blockLocation.top < window.innerHeight){
+		let blockLocation = newBlock[i].getBoundingClientRect();	
+			if((0 < blockLocation.top && blockLocation.top < window.innerHeight) || (0 < blockLocation.bottom && blockLocation.bottom < window.innerHeight)){
 	//Check if it already has this class so we wont repeat animation
 				if(!newBlock[i].classList.contains("seen")){
 					newBlock[i].classList.add("seen")
